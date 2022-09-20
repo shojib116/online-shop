ec4bdb8fd96c0f1ddae7beae449ec2c394580670
Nazmul Hasan <nazmul@nazmul.com>
2022-09-20 07:17:17 +0600

    added error handlers, csrf protection, sessions and cookies

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const path = require('path');
 
 const express = require('express');
+const csrf = require('csurf');
+const expressSession = require('express-session');
 
+const createSessionConfig = require('./config/session');
 const db = require('./data/database');
+const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
 
 const authRoutes = require('./routes/auth.routes');
 
@@ -15,7 +20,17 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({extended: false}));
 
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
+
+app.use(csrf());
+
+app.use(addCsrfTokenMiddleware);
+
 app.use(authRoutes);
+
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
   .then(function() {
