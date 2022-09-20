6f29d8fb390771488f2c54c1393965de58a0a322
Nazmul Hasan <nazmul@nazmul.com>
2022-09-20 20:21:11 +0600

    added logout logic

--- util/authentication.js
+++ util/authentication.js
@@ -3,7 +3,12 @@ function createUserSession(req, user, action) {
   req.session.save(action);
 }
 
+function destroyUserAuthSession(req) {
+  req.session.uid = null;
+}
+
 
 module.exports = {
   createUserSession: createUserSession,
+  destroyUserAuthSession: destroyUserAuthSession,
 }
